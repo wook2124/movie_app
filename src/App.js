ad85b45b5134418a4ea3e1d7963158f6511bccf8
wook2124 <wook2124@naver.com>
2020-03-15 18:15:07 +0900

    #2.3 .map Recap

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 import React from "react";
 
-function Food(props) {
-  return (
-    <div>
-      <h3>I like {props.name}</h3>
-      <img src={props.picture} />
-    </div>
-  );
-}
-
 const favFood = [
   {
+    id: 1,
     name: "Kimchi",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQzoVbwNwurx8xkj0hdIcI6YNbf8okD00EvniBpgZnZAfNWnCWn"
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQzoVbwNwurx8xkj0hdIcI6YNbf8okD00EvniBpgZnZAfNWnCWn"
   },
   {
+    id: 2,
     name: "Ramen",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSHu-fVSkgztuemTL6nqJ-vCSC6Tmv57zbZMQ50DqOy1dAeb7Oi"
   },
   {
+    id: 3,
     name: "Apple",
     image:
       "https://image.shutterstock.com/image-photo/ripe-red-apple-fruit-half-260nw-699645961.jpg"
   },
   {
+    id: 4,
     name: "Pizza",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQOl8_b3AIXb9Ems_wVoD5wKIheN514af-CegpiACUmsVX9KwHf"
   }
 ];
 
+function Food(props) {
+  return (
+    <div>
+      <h3>I like {props.name}</h3>
+      <img src={props.picture} alt={props.name} />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>Hello, I'm renewed!</h1>
       {favFood.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
