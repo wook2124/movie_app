b5afe6f32929d4e214defd6c2c26991640eed8ec
wook2124 <wook2124@naver.com>
2020-03-30 15:18:02 +0900

    Updates

---
+++ precache-manifest.3a020bcf3a868c6cde500887d0b0b1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5004f6db7ef992ebc10ab56c533394",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "08d37c87f463a75c602b",
+    "url": "/movie_app/static/css/main.28a61c39.chunk.css"
+  },
+  {
+    "revision": "5f45f23c97bbf82f1b91",
+    "url": "/movie_app/static/js/2.13bd5e02.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.13bd5e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d37c87f463a75c602b",
+    "url": "/movie_app/static/js/main.8e851213.chunk.js"
+  },
+  {
+    "revision": "c145c45984fe2e41280f",
+    "url": "/movie_app/static/js/runtime-main.ffbc15b4.js"
+  }
+]);
