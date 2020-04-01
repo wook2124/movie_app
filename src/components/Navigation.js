6d022b58d7ca384a64152150352a09388fce3b6d
wook2124 <wook2124@naver.com>
2020-04-01 17:14:24 +0900

    #6.2 Building the Navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </div>
+  );
+}
+
+export default Navigation;
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <span>About this page: I built it because I love movie</span>;
+  return (
+    <div className="about__container">
+      <span>
+        “나는 신발이 없음을 한탄했는데, 길에서 발이 없는 사람을 만났다.”
+      </span>
+      <span>− 데일 카네기</span>
+    </div>
+  );
 }
 
 export default About;
