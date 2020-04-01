4da00bcf047409cf6a3b22205da622d514ac23d9
wook2124 <wook2124@naver.com>
2020-04-01 17:17:46 +0900

    #6.2 Building the Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import "./Navigation.css";
 
 function Navigation() {
   return (
-    <div>
+    <div className="nav">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </div>
